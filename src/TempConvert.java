5275fd1a4ea6cd456dbd9d68ec10963f3b802dbc
clydewbrown <clydewbrown@gmail.com>
2013-08-09 10:45:53 -0400

    Adding java file (with errors) for students

---
+++ src/TempConvert.java
@@ -0,0 +1,18 @@
+import java.until.Scanner;
+public class ThisIsMyClassName 
+
+	public static main(String[] args) {
+		// -------INSTRUCTIONS-------
+		// Fix the errors in this class, to create a working program
+		// Hint: there are TEN errors in total
+		sc = new Scanner(System.in);
+		int tempF; tempC;
+		
+		System.out.print("Enter a temperature in degreez Fahrenheit: ");
+		tempF = sc..nextInt();
+		tempC = (tempF + 32 ) * 5 / 9;
+		
+		System.out.println(tempF + " degrees Fahrenheit equals " tempC + " degrees Celcius.");
+	}
+
+}
